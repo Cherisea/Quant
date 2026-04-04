b1eb7df5fafb2cfd0fa710dd2cc54c2372e65cac
Joey Cherisea <hai.p@northeastern.edu>
2026-04-04 16:52:49 +0800

    Write a function that generates buy and sell signals based on our defined momentum strategy

--- backtest.py
+++ backtest.py
@@ -2,3 +2,29 @@
 Momentum Backtest Engine
 """
 
+import pandas as pd
+from pandas import DataFrame
+from main import TigerClients, TechAnalyst
+
+
+def generate_signals(client: TigerClients, df: DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df['signal'] = 0
+
+    # Find a single bar where fast_ema just crossed above/below slow_ema
+    cross_up = (df['fast_ema'] > df['slow_ema']) & (df['fast_ema'].shift(1) <= df['slow_ema'].shift(1))
+    cross_down = (df['fast_ema'] < df['slow_ema']) & (df['fast_ema'].shift(1) >= df['slow_ema'].shift(1))
+
+    momentum = df['roc'] > client.roc_threshold
+    volume = df['volume'] > client.vol_coefficient * df['vol_ma']
+
+    df.loc[cross_up & momentum & volume, 'signal'] = 1     # Buy signal
+    df.loc[cross_down] = -1        # Sell signal
+
+    return df
+
+client = TigerClients()
+analyst = TechAnalyst(client)
+bars = analyst.fetch_bars(test=True)
+df = analyst.compute_indicators(bars)
+generate_signals(client, df)
