76d266cd65755af067c535a704a40a8fb7ca6c86
Joey Cherisea <hai.p@northeastern.edu>
2026-05-04 16:54:24 +0800

    Tweaks comments and fix a bug in generating trading signals

--- backtest.py
+++ backtest.py
@@ -11,7 +11,7 @@ def generate_signals(client: TigerClients, df: DataFrame) -> pd.DataFrame:
     df = df.copy()
     df['signal'] = 0
 
-    # Find a single bar where fast_ema just crossed above/below slow_ema
+    # Moment of transition where fast and slow ema flips position
     cross_up = (df['fast_ema'] > df['slow_ema']) & (df['fast_ema'].shift(1) <= df['slow_ema'].shift(1))
     cross_down = (df['fast_ema'] < df['slow_ema']) & (df['fast_ema'].shift(1) >= df['slow_ema'].shift(1))
 
@@ -19,7 +19,7 @@ def generate_signals(client: TigerClients, df: DataFrame) -> pd.DataFrame:
     volume = df['volume'] > client.vol_coefficient * df['vol_ma']
 
     df.loc[cross_up & momentum & volume, 'signal'] = 1     # Buy signal
-    df.loc[cross_down] = -1        # Sell signal
+    df.loc[cross_down, "signal"] = -1        # Sell signal
 
     return df
 
