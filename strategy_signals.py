3233d38df62e4db2a5d26d423eebeac4de077b4a
Joey Cherisea <hai.p@northeastern.edu>
2026-05-29 11:05:44 +0800

    Add another two helper functions that provides a unified interface for trading signals

--- strategy_signals.py
+++ strategy_signals.py
@@ -5,6 +5,8 @@ from configs import StrategySettings
 
 # Unified int signal code used internally
 HOLD, BUY, SELL = 0, 1, -1
+_SIGNAL_LABELS = {0: "HOLD", 1: "BUY", -1: "SELL"}
+
 
 def compute_signals(df: pd.DataFrame, strategy: StrategySettings) -> pd.Series:
     """ Vectorized EMA crossover signals for every row, complemented by a rate of change
@@ -40,9 +42,31 @@ def compute_signals(df: pd.DataFrame, strategy: StrategySettings) -> pd.Series:
     roc = df["roc"] > strategy.roc_threshold
     vol = df["volume"] > strategy.vol_coefficient * df["vol_ma"]
 
-    signals = pd.Series(HOLD, index=df.index, dtype="int8")
-    signals.loc[cross_up & (roc | vol)] = BUY
+    signals = pd.Series(0, index=df.index, dtype="int8")
+    signals.loc[cross_up & (roc | vol)] = 1
 
     # TODO: is a cross-down signal sufficient for a sell signal
-    signals.loc[cross_down] = SELL
+    signals.loc[cross_down] = -1
     return signals
+
+def get_lastest_signal(df: pd.DataFrame, strategy: StrategySettings) -> int:
+    """ Retrieve the last trading signal from a series of signals.
+    """
+    if len(df) < 2:
+        return 0
+    
+    return compute_signals(df, strategy).iloc[-1]
+
+def convert_code_to_label(code: int) -> str:
+    """ Map numeric code of trading signal to a human-readable label.
+
+    Args:
+        code: signal code with values from [0, -1, 1].
+
+    Returns:
+        a trading label of either BUY, SELL or HOLD. Defaults to HOLD if 
+        code if not valid.
+    """
+    return _SIGNAL_LABELS.get(code, "HOLD")
+
+
