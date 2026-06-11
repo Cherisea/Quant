0299b5c6fbf6300cd27d867a1b0177efebb59ecd
Joey Cherisea <hai.p@northeastern.edu>
2026-06-11 20:48:03 +0800

    Replace a custom function with a dataclass

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 """
 Utility functions to support the execution of primary scripts.
 """
-import sys
 import math
-import logging
+import logging.config
 
 from configs import HKPlatformFeePlan, TradeFeesHK
 
@@ -14,24 +13,18 @@ def setup_logging(log_file: str, level: str = "INFO"):
         log_file: name of file to store logs
         level: severity of information to be logged
     """
-    root = logging.getLogger()
-
-    # Avoid duplicate handlers 
-    if root.handlers:
-        return
-    
-    root.setLevel(getattr(logging, level.upper(), "INFO"))
-    fmt = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-    
-    # Configure handlers
-    fh = logging.FileHandler(log_file)
-    fh.setFormatter(fmt)
-    sh = logging.StreamHandler(sys.stdout)
-    sh.setFormatter(fmt)
-
-    # Attach handlers to logger
-    root.addHandler(fh)
-    root.addHandler(sh)
+    logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {"format": "%(asctime)s [%(levelname)s] %(message)s"},
+        },
+        "handlers": {
+            "file":    {"class": "logging.FileHandler",   "filename": log_file, "formatter": "standard"},
+            "console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout", "formatter": "standard"},
+        },
+        "root": {"level": level.upper(), "handlers": ["file", "console"]},
+    })
 
 def round_to_lot(lot_size, qty: int) -> int:
         """Round requested quantity to a multiple of lot size.
