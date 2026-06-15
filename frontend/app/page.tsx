9c0adb7bea942e706ff68a47207672d15f042abc
Joey Cherisea <hai.p@northeastern.edu>
2026-06-15 21:29:34 +0800

    Implement dashboard page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,17 @@
+import StatusBar from "@/components/dashboard/StatusBar";
+import EquityChart from "@/components/dashboard/EquityChart";
+import KpiGrid from "@/components/dashboard/kpiGrid";
+import TradeLog from "@/components/dashboard/TradeLog";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <StatusBar />
+      <div style={{ padding: 20 }}>
+        <EquityChart />
+        <KpiGrid />
+        <TradeLog />
+      </div>
+    </>
+  )
+}
