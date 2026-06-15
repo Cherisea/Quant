fce9e47e81f90ac75c5948fd9aac1b5aab6732e7
Joey Cherisea <hai.p@northeastern.edu>
2026-06-15 21:30:40 +0800

    Fix a case error in imports

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import StatusBar from "@/components/dashboard/StatusBar";
 import EquityChart from "@/components/dashboard/EquityChart";
-import KpiGrid from "@/components/dashboard/kpiGrid";
+import KpiGrid from "@/components/dashboard/KpiGrid";
 import TradeLog from "@/components/dashboard/TradeLog";
 
 export default function DashboardPage() {
