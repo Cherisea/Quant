4e16adcb04a29d687f9694e9ae75d14d3de42539
Joey Cherisea <hai.p@northeastern.edu>
2026-06-15 20:54:40 +0800

    Add imports in dashboard overview page and inject placeholder code in components files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,5 @@
+import StatusBar from "@/components/dashboard/StatusBar";
+import EquityChart from "@/components/dashboard/EquityChart";
+import KpiGrid from "@/components/dashboard/KpiGrid";
+import TradeLog from "@/components/dashboard/TradeLog";
+
