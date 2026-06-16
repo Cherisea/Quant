ac1f722d48305f2d204b4489369cb80ddeffd3dc
Joey Cherisea <hai.p@northeastern.edu>
2026-06-16 10:09:50 +0800

    Add some starter code to the script

--- frontend/components/dashboard/StatusBar.tsx
+++ frontend/components/dashboard/StatusBar.tsx
@@ -1,5 +1,11 @@
 "use client";
+import { useTradingContext } from "@/context/TradingContext";
+
+function Divider() {
+    return <div style={{ width:1, height:24, background: "white", flexShrink:0 }} />;
+}
 
 export default function StatusBar() {
-    
+    const { price, position } = useTradingContext();
+
 }
