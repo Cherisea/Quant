5e1a7ed5f212bea50dfb2fc8f5869177d6b7db47
Joey Cherisea <hai.p@northeastern.edu>
2026-06-18 14:36:31 +0800

    Write a page for Backtest

---
+++ frontend/app/backtest/page.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "@/components/ui/PageHeader";
+
+export default function BacktestPage() {
+    return (
+        <div style={{ padding: 20 }}>
+                <PageHeader title="Backtest" sub="Run your strategy against historical price data" />
+        </div>
+    )
+}
