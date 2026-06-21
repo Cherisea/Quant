f57c21c87d7909af05b0c7adf5ed5b5e39e84ccb
Joey Cherisea <hai.p@northeastern.edu>
2026-06-21 21:46:55 +0800

    Stack a new nav bar on top of sidebar in app shell

--- frontend/components/layout/AppShell.tsx
+++ frontend/components/layout/AppShell.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import SideBar from "./SideBar";
+import NavBar from "./NavBar";
 import { T } from "@/lib/theme";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
     return (
-        <div style={{ display: "flex", minHeight: "100vh", background: T.bg, color: T.text }}>
-            <SideBar />
-            <main style={{ flex: 1, minWidth: 0 }}>
-                {children}
-            </main>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", background: T.bg, color: T.text }}>
+            <NavBar />
+            <div style={{ display: "flex", flex: 1 }}>
+                <SideBar />
+                <main style={{ flex: 1, minWidth: 0 }}>
+                    {children}
+                </main>
+            </div>
         </div>
     );
 }
