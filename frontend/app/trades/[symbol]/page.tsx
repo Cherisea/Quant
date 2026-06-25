9c689e33efef232c0e7cfc6e0d5eb0efd5bcc0ba
Joey Cherisea <hai.p@northeastern.edu>
2026-06-25 17:46:39 +0800

    Create two new scripts for the trade history page linked to a postion card

---
+++ frontend/app/trades/[symbol]/page.tsx
@@ -0,0 +1,9 @@
+import TradeHistoryView from "@/components/trades/TradeHistoryView";
+
+// Extract dynamic query values from url
+export default function TradeHistoryPage(
+    {params} : {params:{symbol:string};}) 
+{
+    return <TradeHistoryView symbol={params.symbol}/>
+}
+
